149a56d5a320af20cc93aa6883cc2ce8a3d9b9cd
Hien Pham <62068606+hienminhpham0101@users.noreply.github.com>
2021-04-15 23:23:55 +0700

    add-layout

---
+++ DTT.LRM.Web/Controllers/TaiSan/TaiSanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DTT.LRM.Web.Controllers.TaiSan
+{
+    public class TaiSanController : Controller
+    {
+        // GET: TaiSan
+        public ActionResult Index()
+        {
+            return View("~/App/Main/views/TaiSan/index.cshtml");
+        }
+    }
+}
